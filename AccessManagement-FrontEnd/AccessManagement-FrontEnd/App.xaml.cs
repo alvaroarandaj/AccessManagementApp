cdd2174e9139acfa0b155c3ca845c51b40735eaa
alvaroarandaj <alvaro.arandaj@gmail.com>
2024-10-25 19:13:07 +0200

    Front end commit

---
+++ AccessManagement-FrontEnd/AccessManagement-FrontEnd/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AccessManagement_FrontEnd
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AccessManagement-FrontEnd/AccessManagement-FrontEnd/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessManagement_FrontEnd
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
